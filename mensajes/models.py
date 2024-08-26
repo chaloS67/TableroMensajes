785890748d35c0d70d583f2dee9ce3dc5414240a
chaloS67 <chalosampini@hotmail.com>
2024-08-26 14:23:47 -0300

    se creo la base de datos tablero con su correspondiente migracion

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# models.py
 
-# Create your models here.
+class Tablero(models.Model):
+    texto = models.CharField(max_length=200),
+    remitente = models.CharField(max_length=50),
+    destinatario = models.CharField(max_length=10, choices=[('pendiente', 'Pendiente'),
+    ('completado', 'Completado')]),
+
+def __str__(self):
+    return self.titulo
