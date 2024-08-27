6e85276fe43a9e4a2bfb1afdb5a2362c6303fe47
chaloS67 <chalosampini@hotmail.com>
2024-08-27 16:12:42 -0300

    correccion del modelo

--- mensajes/models.py
+++ mensajes/models.py
@@ -2,10 +2,10 @@ from django.db import models
 # models.py
 
 class Tablero(models.Model):
-    texto = models.CharField(max_length=200),
-    remitente = models.CharField(max_length=50),
-    destinatario = models.CharField(max_length=10, choices=[('pendiente', 'Pendiente'),
-    ('completado', 'Completado')]),
-
+    texto = models.TextField()
+    remitente = models.CharField(max_length=50)
+    destinatario = models.CharField(max_length=50)
+    
 def __str__(self):
     return self.titulo
+
